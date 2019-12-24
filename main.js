01a5e5398d71a2dcdb05d3254b6c203ba7d1b943
MapGIS <9409101+MapGIS-Curran@users.noreply.github.com>
2019-12-23 21:59:40 -0900

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
 require(["esri/views/SceneView", "esri/WebMap", "esri/widgets/LayerList"], function(SceneView, WebMap, LayerList) {
-        /************************************************************
-         * Creates a new WebMap instance. A WebMap must reference
-         * a PortalItem ID that represents a WebMap saved to
-         * arcgis.com or an on-premise portal.
-         *
-         * To load a WebMap from an on-premise portal, set the portal
-         * url with esriConfig.portalUrl.
-         ************************************************************/
+//Creates a new WebMap instance. A WebMap must reference a PortalItem ID that represents a WebMap saved to arcgis.com or an on-premise portal.
         var webmap = new WebMap({
           portalItem: {
             // autocasts as new PortalItem()
@@ -14,16 +7,13 @@ require(["esri/views/SceneView", "esri/WebMap", "esri/widgets/LayerList"], funct
           }
         });
 
-        /************************************************************
-         * Set the WebMap instance to the map property in a MapView.
-         ************************************************************/
+        //Set the WebMap instance to the map property in a SceneView.
         var view = new SceneView({
           map: webmap,
           container: "viewDiv"
         });
-		
-		 // Add a legend instance to the panel of a
-        // ListItem in a LayerList instance
+
+	// Add a legend instance to the panel of a ListItem in a LayerList instance
         const layerList = new LayerList({
           view: view,
           listItemCreatedFunction: function(event) {
